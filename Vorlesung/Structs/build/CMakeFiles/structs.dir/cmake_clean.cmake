da0454f482d65c6fd7fd244a74ded19fa7e50e77
Max-LKn <122216817+Max-LKn@users.noreply.github.com>
2023-04-19 07:24:15 +0000

    Setup and struct example

---
+++ Vorlesung/Structs/build/CMakeFiles/structs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/structs.dir/structs.cpp.o"
+  "CMakeFiles/structs.dir/structs.cpp.o.d"
+  "structs"
+  "structs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/structs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

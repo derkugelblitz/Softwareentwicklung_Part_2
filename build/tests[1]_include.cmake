034cf9ee0939ea8cf0df415133b8fdc6b516fadf
derkugelblitz <122254574+derkugelblitz@users.noreply.github.com>
2023-05-10 06:23:23 +0000

    updates and new things

---
+++ build/tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/workspaces/Softwareentwicklung_Part_2/build/tests[1]_tests.cmake")
+  include("/workspaces/Softwareentwicklung_Part_2/build/tests[1]_tests.cmake")
+else()
+  add_test(tests_NOT_BUILT tests_NOT_BUILT)
+endif()
